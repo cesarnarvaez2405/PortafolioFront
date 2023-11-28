b5a65f23e277593cf8fb81725da36e2339cfa9fe
cesarnarvaez2405 <cesar.teacher24@gmail.com>
2023-11-27 23:51:55 -0500

    Ajustes sin terminar del auth

--- src/services/usuarios.js
+++ src/services/usuarios.js
@@ -1,11 +1,35 @@
 import axios from "axios";
 import API from "../api/apiNode";
 
+axios.interceptors.request.use(request => {
+  if(request.headers.authorization == '' ) {
+    request.headers.authorization = localStorage.getItem('token')
+  }
+  return request
+})
+
 let response = [];
+
+/**
+ * Modulo para realizar las peticiones http API
+ * @module export - Functions
+ */
 
 export default {
   async login(body) {
-    console.log(body);
-    response = await axios.post(`${API.node}portafolio/v1/auth/login`, body);
+    try {
+      console.log(body);
+      response = await axios.post(`${API.node}portafolio/v1/auth/login`, body,
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+      return response.data
+
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
--- src/views/Auth/hooks/autenticador.jsx
+++ src/views/Auth/hooks/autenticador.jsx
@@ -13,7 +13,6 @@ export const Autenticador = () => {
       email,
       password,
     };
-    console.log(usuario);
     await usuarios.login(usuario);
   };
 
